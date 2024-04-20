bdc0d443610c78c2f09b759a0f3ed4c0449a9ae6
yogigyle <67333949+yogigyle@users.noreply.github.com>
2024-04-20 20:56:49 +0300

    implement formeasy v0.2

--- assets/js/formeasy.js
+++ assets/js/formeasy.js
@@ -1,16 +1,23 @@
 // *** [AVIRAM] JS code to integrate FormEasy into website, see: https://github.com/Basharath/FormEasy ***
 // const siteKey = '<YOUR_SITE_KEY>';
-
 // const url = 'https://script.google.com/macros/s/AKfycbxo0crdRI619kf-99RZIrq7XNDbn-5bxLpN50JyyMZlrRnZSJjGuRY-Z1O96jlmh4TkyQ/exec'; // {{ site.formeasy.url }}
 
-function handleSubmit(event) {
-  event.preventDefault();
+// function handleSubmit(event) {
+//   event.preventDefault();
+function submitForm() {
+  var data = {
+    "name": document.getElementById('name').value,
+    "email": document.getElementById('email').value,
+    "message": document.getElementById('message').value
+  };
 
-  data.name = document.getElementById('name').value;
-  data.email = document.getElementById('email').value;
-  data.message = document.getElementById('message').value;
+  // data.name = document.getElementById('name').value;
+  // data.email = document.getElementById('email').value;
+  // data.message = document.getElementById('message').value;
 
-  fetch(formEasyUrl, {
+  const url = 'https://script.google.com/macros/s/AKfycbxo0crdRI619kf-99RZIrq7XNDbn-5bxLpN50JyyMZlrRnZSJjGuRY-Z1O96jlmh4TkyQ/exec';
+
+  fetch(url, {
     method: 'POST',
     headers: {
       'Content-Type': 'text/plain;charset=utf-8',
@@ -22,4 +29,4 @@ function handleSubmit(event) {
     .catch((err) => console.log('err', err));
 }
 
-document.getElementById('contact_formeasy').addEventListener('submit', handleSubmit);
+// document.getElementById('contact_formeasy').addEventListener('submit', handleSubmit);
